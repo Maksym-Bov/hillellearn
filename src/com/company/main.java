40509cee3777df6021668a74d6859f396b6be7ce
Maksym <logggan25@gmail.com>
2022-01-20 08:32:13 +0200

    update

--- src/com/company/main.java
+++ src/com/company/main.java
@@ -4,63 +4,14 @@ import java.util.Scanner;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        String str = "S[AM HARRIS]";
+        str = str.toUpperCase();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Input day ");
-        int DayNumber = scanner.nextInt();
-        //int hour = scanner.nextInt();
-        /*
-        String textHour =  (hour > 18) ? "Good evening"  : "Good day" ;
-
-        System.out.println(textHour);
-
-        if(hour < 24 && hour > 0){
-        if (hour > 18) {
-            System.out.println("Good evening");
-
-        }
-        else if (hour > 10) {
-
-            System.out.println("Good morning");
-
-        }
-        else {
-            System.out.println("Good day");
-        }
-       }
-        else {
-            System.out.println("error" + hour + "is not more 24 hour");
-        }*/
-        switch (DayNumber){
-            case 1 :
-                System.out.println("Monday");
-                break;
-            case 2 :
-                System.out.println("Tuesday");
-                break;
-            case 3 :
-                System.out.println("Wednesday");
-                break;
-            case 4 :
-                System.out.println("Thursday");
-                break;
-            case 5 :
-                System.out.println("Friday");
-                break;
-            case 6 :
-                System.out.println("Saturday");
-                break;
-            case 7 :
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Error day");
-        }
-
-
+        String []  arrstr = str.split(" ");
+        System.out.print(arrstr[0].substring(0,1) + "." + arrstr[1].substring(0,1) + "." );
     }
-
-
 }
+
+
+
